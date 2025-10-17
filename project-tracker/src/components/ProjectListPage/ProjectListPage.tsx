6d3e0a62422520b0d86249d6340d606d16525665
susshikan <muhamadh123g@gmail.com>
2025-10-18 04:18:46 +0700

    fix flexbox

--- project-tracker/src/components/ProjectListPage/ProjectListPage.tsx
+++ project-tracker/src/components/ProjectListPage/ProjectListPage.tsx
@@ -3,6 +3,18 @@ import type { Project } from "./TableCard";
 import TableCard from "./TableCard";
 //flex items-center justify-center min-h-screen bg-background p-4 flex-col
  
+export default function ProjectListPage(){ 
+    return( 
+        <div className="item-center"> 
+        <div className="flex justify-center min-h-screen p-4 flex-col"> 
+            <div className="pl-4"> 
+                <AddButton /> 
+            </div> 
+            <TableCard projects={demoProjects}/> 
+        </div> 
+        </div> 
+    ) 
+}
 
 export const demoProjects: Project[] = [
   { id: "1", name: "Landing Page Revamp", tags: ["frontend", "tailwind", "a11y"] },

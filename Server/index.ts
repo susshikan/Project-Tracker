68cae117a6ab4c639046ed63dfb8f4ec36161d1b
susshikan <muhamadh123g@gmail.com>
2025-10-27 21:05:20 +0700

    Solve bug in prisma

---
+++ Server/index.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from '@prisma/client'
+import express, {Request, Response} from "express";
+import cors from "cors"
+
+const port = 3001;
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(cors({
+    origin: 'https://link-lux.vercel.app'
+}));
+app.use(express.json());

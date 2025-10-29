5a684def4a935becfd3065cedb2b2be9121fc8ae
susshikan <muhamadh123g@gmail.com>
2025-10-29 12:28:49 +0700

    Add auth

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '../generated/prisma'
 import express, {Request, Response} from "express";
 import cors from "cors"
 import userRouter from './routes/userRoutes';
+import authRouter from './routes/authRoutes';
 
 const port = 3001;
 const app = express();
@@ -10,6 +11,7 @@ const prisma = new PrismaClient();
 app.use(cors({}));
 app.use(express.json());
 
+app.use('/api/auth', authRouter)
 app.use('/api/users', userRouter)
 
 app.listen(port, () => {
